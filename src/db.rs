09db6b5e03975fe44ae7988db9506e46f7229138
reillymc <reilly@mackenzie-cree.net>
2024-09-22 04:41:59 +0000

    Simplify DB environment variables

--- src/db.rs
+++ src/db.rs
@@ -8,7 +8,13 @@ const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db_user = env::var("POSTGRES_USER").expect("POSTGRES_USER must be set");
+    let db_password = env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD must be set");
+    let db_name = env::var("POSTGRES_DB").expect("POSTGRES_DB must be set");
+    let db_host = env::var("DB_HOST").expect("DB_HOST must be set");
+    let db_port = env::var("PGPORT").expect("PGPORT must be set");
+
+    let database_url = format!("postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {database_url}"))
 }
